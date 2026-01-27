42cbfa0d12eaf6b90950a5e6b204cc7cdd9e0b28
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-27 11:36:07 +0100

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return;
+  return (
+    <main>
+      <h1>Discover what is possible with 3D printing</h1>
+      <p>
+        Join our community of creators and explore a vast library of
+        user-submitted models.
+      </p>
+      <button>Browse Models</button>
+      <img
+        src="/home-page-hero-img.png"
+        alt="A colorful array of 3D-printed objects including geometric shapes,
+      miniatures, and functional items arranged on a clean white surface,
+      showcasing the creative possibilities of 3D printing technology"
+      />
+    </main>
+  );
 }
