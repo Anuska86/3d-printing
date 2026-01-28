fb262c48e1180f8ea4178812773b020528cd23e0
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-28 11:57:01 +0100

    active link

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import { NavLinkProps } from "../utils/types";
+import { usePathname } from "next/navigation";
 
 export default function NavLink({ href, children }: NavLinkProps) {
+  const pathname = usePathname();
+
+  //check the name of the URL
+  const isActive = pathname.startsWith(href);
+
   return (
     <li className="text-sm uppercase">
       <Link
         href={href}
-        className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:bg-brand hover:text-white"
+        className={`px-4 py-2 transition-colors rounded-md cursor-pointer ${
+          isActive
+            ? "bg-brand text-white" // Active styles
+            : "text-gray-700 hover:bg-brand hover:text-white" // Inactive styles
+        }`}
       >
         {children}
       </Link>
