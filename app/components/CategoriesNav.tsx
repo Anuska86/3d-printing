88e100f04ce71d6fd1acdaf45db833014dac1a44
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-28 16:03:22 +0100

    CategoriesNav

---
+++ app/components/CategoriesNav.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
+import { getAllCategories } from "../utils/lib/categories";
+import { Category } from "../utils/types";
+
+export default function CategoriesNav() {
+  const pathname = usePathname();
+  const categories: Category[] = getAllCategories();
+  console.log(categories);
+
+  return (
+    <aside className="sticky top-0 z-10 w-full bg-white border-b border-gray-200   md:-translate-y-1/2 md:border-none">
+      <div className="relative">
+        <nav className="w-full overflow-x-auto  scrollbar-hide">
+          <ul className="flex px-4 py-3 space-x-4 whitespace-nowrap md:p-4">
+            <NavLink href="/models" isActive={pathname === "/models"}>
+              All
+            </NavLink>
+            {categories.map((item) => (
+              <NavLink
+                href={`/models/categories/${item.slug}`}
+                isActive={pathname === "/models/categories/${item.slug}"}
+                key={item.slug}
+              >
+                {item.displayName}
+              </NavLink>
+            ))}
+          </ul>
+        </nav>
+        {/* Fading edge/gradient for horizontal scroll hint on mobile */}
+        <div className="absolute top-0 right-0 w-8 h-full pointer-events-none  bg-linear-to-1 from-white to-transparent md:hidden" />
+      </div>
+    </aside>
+  );
+}
--- app/models/layout.tsx
+++ app/models/layout.tsx
@@ -1,38 +1,13 @@
 import { ReactNode } from "react";
-import { getAllCategories } from "../utils/lib/categories";
-import { Category } from "../utils/types";
-import NavLink from "../components/NavLink";
+import CategoriesNav from "../components/CategoriesNav";
 
 export default function ModelsLayout({ children }: { children: ReactNode }) {
-  const categories: Category[] = getAllCategories();
-  console.log(categories);
   return (
     <div className="relative flex flex-col min-h-screen">
-      {/* Responsive Navigation */}
-      <aside className="sticky top-0 z-10 w-full bg-white border-b border-gray-200   md:-translate-y-1/2 md:border-none">
-        <div className="relative">
-          <nav className="w-full overflow-x-auto  scrollbar-hide">
-            <ul className="flex px-4 py-3 space-x-4 whitespace-nowrap md:p-4">
-              <NavLink href="/models">All</NavLink>
-              {categories.map((category) => (
-                <NavLink
-                  href={`/models/categories/${category.slug}`}
-                  key={category.slug}
-                >
-                  {category.displayName}
-                </NavLink>
-              ))}
-            </ul>
-          </nav>
-          {/* Fading edge/gradient for horizontal scroll hint on mobile */}
-          <div className="absolute top-0 right-0 w-8 h-full pointer-events-none bg-gradient-to-l from-white to-transparent md:hidden" />
-        </div>
-      </aside>
+      <CategoriesNav />
+
       {/* Main Content Area */}
-      <main className="flex-1 p-4 md:ml-64">
-        {" "}
-        {children}
-      </main>
+      <main className="flex-1 p-4 md:ml-64"> {children}</main>
     </div>
   );
 }
