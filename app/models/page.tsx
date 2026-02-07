62984ed93c92465336d2acb70f3810eb61e2e591
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-07 18:34:41 +0100

    search

--- app/models/page.tsx
+++ app/models/page.tsx
@@ -1,7 +1,23 @@
 import ModelsGrid from "../components/ModelsGrid";
+import type { ModelsPageProps } from "../utils/types";
 import { getModels } from "../utils/lib/models";
 
-export default async function Page() {
+export default async function Page({searchParams}) {
   const models = await getModels();
-  return <ModelsGrid title="3D Models" models={models} />;
+  return (
+    <div>
+      <form className="search-form">
+        <label htmlFor="3d-model" className="sr-only"></label>
+        <input
+          type="search"
+          id="3d-model-search"
+          name="query"
+          placeholder="Search for 3d models..."
+          className="search-input"
+          autoComplete="off"
+        ></input>
+      </form>
+      <ModelsGrid title="3D Models" models={models} />;
+    </div>
+  );
 }
